defe688e4a2280f38520f75607088926bcdcd572
Nadybee <natalie@yoodlize.com>
2022-07-30 10:36:55 -0600

    change

--- seeds/index.js
+++ seeds/index.js
@@ -8,7 +8,7 @@ const seedWeddings = require('./weddingSeeds');
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
-  await sequelize.sync({ force: false });
+  await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
 
   await seedUsers();
