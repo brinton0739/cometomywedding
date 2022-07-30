285d1a7b90beeb385cece891cb1778dc3134acf4
Nadybee <natalie@yoodlize.com>
2022-07-30 10:56:57 -0600

    antoehr

--- models/index.js
+++ models/index.js
@@ -1,49 +1,3 @@
-<<<<<<< HEAD
-// const User = require('./User');
-// const Guest = require('./Event');
-// const Location = require('./Location');
-// const Wedding = require('./Wedding');
-// const Registry = require('./Registry');
-// const Event = require('./Event');
-
-
-// User.hasMany(Guest, {
-//     foreignKey: 'guest_id',
-//     onDelete: 'CASCADE'
-// });
-
-// Wedding.hasMany(Guest, {
-//     foreignKey: 'wedding_id',
-//     onDelete: 'CASCADE'
-// });
-
-// Wedding.hasOne(Registry, {
-//     foreignKey: 'wedding_id'
-// });
-
-// Guest.belongsTo(User, {
-//     foreignKey: 'user_id'
-// });
-
-// Guest.belongsTo(Wedding, {
-//     foreignKey: 'wedding_id'
-// });
-
-// Location.hasMany(Event, {
-//     foreignKey: 'location_id',
-//     onDelete: 'CASCADE'
-// });
-
-// Event.hasOne(Location, {
-//     foreignKey: 'location_id'
-// });
-
-// Registry.belongsTo(Wedding, {
-//     foreignKey: 'wedding_id'
-// });
-
-// module.exports = { User, Guest, Location, Wedding, Registry, Event };
-=======
 const User = require('./User');
 const Guest = require('./Event');
 const Location = require('./Location');
@@ -54,12 +8,12 @@ const Event = require('./Event');
 
 User.hasMany(Guest, {
     foreignKey: 'guest_id',
-    // onDelete: 'CASCADE'
+    onDelete: 'CASCADE'
 });
 
 Wedding.hasMany(Guest, {
     foreignKey: 'wedding_id',
-    // onDelete: 'CASCADE'
+    onDelete: 'CASCADE'
 });
 
 Wedding.hasOne(Registry, {
@@ -76,7 +30,7 @@ Guest.belongsTo(Wedding, {
 
 Location.hasMany(Event, {
     foreignKey: 'location_id',
-    // onDelete: 'CASCADE'
+    onDelete: 'CASCADE'
 });
 
 Event.hasOne(Location, {
@@ -88,4 +42,3 @@ Registry.belongsTo(Wedding, {
 });
 
 module.exports = { User, Guest, Location, Wedding, Registry, Event };
->>>>>>> main
