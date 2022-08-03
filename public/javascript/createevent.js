a529816c5e1af98c49e89fd953d7af6da0545e98
Brinton Cole <brintoncole@gmail.com>
2022-08-02 22:05:59 -0600

    fixed the handlebars page for create event

--- public/javascript/createevent.js
+++ public/javascript/createevent.js
@@ -1,32 +1,32 @@
 // handles user signing up for the first time
 const createEventFormHandler = async (event) => {
-    event.preventDefault()
+  event.preventDefault()
     //select users input
     const name = document.querySelector("#event-name").value.trim()
     const date = document.querySelector("#event-date").value.trim()
     const time = document.querySelector("#event-time").value.trim()
     const venue = document.querySelector("#location-name").value.trim()
     const address = document.querySelector("#location-address").value.trim()
-    // const location = document.querySelector("#location-name").value.trim()
     const dress_code =document.querySelector("#dress-code").value.trim()
-
-
+  
   
     if (name && date && time && venue && address && dress_code) {
+
       const response = await fetch("/api/create-event", {
         method: "POST",
-        body: JSON.stringify({ name, date, time, email, venue, address, dress_code }),
+        body: JSON.stringify({ name, date, time, venue, address, dress_code }),
+
         headers: { "Content-Type": "application/json" },
       })
   
       if (response.ok) {
-        // document.location.replace("/")
+        document.location.replace("/dashboard")
       } else {
         alert("Failed to create event.")
       }
     }
-  }
+   }
   
   document
-    .querySelector("#create-event")
+    .querySelector(".justdoit")
     .addEventListener("submit", createEventFormHandler)
