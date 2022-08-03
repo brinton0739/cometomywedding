258e3e0e4c6dcee45255d1c3b97931d9f2564718
TPerschon <timperschon@gmail.com>
2022-08-02 19:03:21 -0600

    freakin' merge fix

--- server.js
+++ server.js
@@ -3,13 +3,10 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const { Event } = require('./models/index')
-
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
-const { s3, generateUploadURL} = require('./config/s3js');
 
 
 const app = express()
@@ -41,29 +38,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
-
-<<<<<<< HEAD
-app.delete("/event/delete/:event", async (req, res) => {
-  console.log("trying")
-  try {
-    const event = await Event.findOne({
-      where: {
-        id: req.params.event
-      }
-    })
-    event.destroy();
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
-=======
-app.get('/s3Url', async (req, res)=> {
-    console.log(generateUploadURL())
-    const url = await generateUploadURL()
-    res.send({url})
-})
->>>>>>> a79ee2e62fdabc455035893cf4021eb20296fb9b
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
