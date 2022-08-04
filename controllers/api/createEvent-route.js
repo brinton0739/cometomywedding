eba8fe5103cf3aeb48857432b1fdbbf5745ea625
TPerschon <timperschon@gmail.com>
2022-08-04 10:50:32 -0600

    Some cleanup, added back button to album page

--- controllers/api/createEvent-route.js
+++ controllers/api/createEvent-route.js
@@ -27,11 +27,7 @@ router.post("/", async (req, res) => {
         address: req.body.address,
         dress_code: req.body.dress_code,
         wedding_id: weddingId,
-      })
-      // req.session.save(() => {
-      //   req.session.guest_id = dbEventData.guest_id,
-      //   res.status(200).json(dbEventData)
-      // })
+      });
       res.status(200).json(dbEventData)
     } catch (err) {
       console.log(err)
--- utils/getWedding 2.js
+++
@@ -1,8 +0,0 @@
-const { Wedding } = require('../models/index');
-
-async function getWedding(wed) {
-    const weddingData = await Wedding.findByPk(wed);
-    return weddingData.get({ plain: true });
-};
-
-module.exports = getWedding;
